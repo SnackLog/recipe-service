d45f8ad26b222fee70694eb51bebea51dfc6a742
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2026-01-21 13:23:35 +0100

    Added input validation via gin

--- internal/database/models/ingredient.go
+++ internal/database/models/ingredient.go
@@ -2,12 +2,12 @@ package models
 
 type Ingredient struct {
 	Id           int     `db:"id" json:"-"`
-	IngredientId int     `db:"ingredient_id" json:"ingredient_id"`
-	Quantity     float64 `db:"quantity" json:"quantity"`
+	IngredientId int     `db:"ingredient_id" json:"ingredient_id" binding:"required,min=1"`
+	Quantity     float64 `db:"quantity" json:"quantity" binding:"required"`
 }
 
 type CustomIngredient struct {
 	Id                 int     `db:"id" json:"-"`
-	CustomIngredientId int     `db:"custom_ingredient_id" json:"custom_ingredient_id"`
-	Quantity           float64 `db:"quantity" json:"quantity"`
+	CustomIngredientId int     `db:"custom_ingredient_id" json:"custom_ingredient_id" binding:"required,min=1"`
+	Quantity           float64 `db:"quantity" json:"quantity" binding:"required"`
 }
--- internal/database/models/recipe.go
+++ internal/database/models/recipe.go
@@ -4,8 +4,8 @@ import "time"
 
 type Recipe struct {
 	Id                int                `db:"id" json:"-"`
-	Name              string             `db:"name" json:"name"`
-	Unit              string             `db:"unit" json:"unit"`
+	Name              string             `db:"name" json:"name" binding:"required,min=1,max=100"`
+	Unit              string             `db:"unit" json:"unit" binding:"required,min=1,max=50"`
 	Username          string             `db:"username" json:"-"`
 	CreatedAt         time.Time          `db:"created_at" json:"created_at"`
 	Ingredients       []Ingredient       `db:"-" json:"ingredients"`
