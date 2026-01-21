dad5be2dc8ef099760dfa6d04122d22b4dfa532b
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2026-01-21 16:01:29 +0100

    Add PUT endpoint to allow for replacing recipies in-place

--- internal/database/recipes/delete.go
+++ internal/database/recipes/delete.go
@@ -2,6 +2,13 @@ package recipes
 
 import "database/sql"
 
+func DeleteRecipeTx(tx *sql.Tx, recipeID int, username string) error {
+	// Delete the recipe from the recipes table, cascading deletes will handle related entries
+	query := "DELETE FROM recipes WHERE id = $1 AND username = $2"
+	_, err := tx.Exec(query, recipeID, username)
+	return err
+}
+
 func DeleteRecipe(db *sql.DB, recipeID int, username string) error {
 	// Delete the recipe from the recipes table, cascading deletes will handle related entries
 	query := "DELETE FROM recipes WHERE id = $1 AND username = $2"
