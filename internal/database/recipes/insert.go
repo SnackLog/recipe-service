ba9ee10b87dc63cd62db0fd1d4d0c2215f313694
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2026-01-21 12:56:56 +0100

    Wrapped recipe insertion into transaction

--- internal/database/recipes/insert.go
+++ internal/database/recipes/insert.go
@@ -8,38 +8,49 @@ import (
 )
 
 func Insert(db *sql.DB, recipe *models.Recipe) (int, error) {
-	recipeId, err := insertRecipe(db, recipe)
+	tx, err := db.Begin()
+	if err != nil {
+		return -1, fmt.Errorf("Unable to begin transaction: %v", err)
+	}
+	defer tx.Rollback()
+
+	recipeId, err := insertRecipe(tx, recipe)
 	if err != nil {
 		return -1, fmt.Errorf("Unable to insert recipe: %v", err)
 	}
 
-	err = insertIngredients(db, recipeId, recipe.Ingredients)
+	err = insertIngredients(tx, recipeId, recipe.Ingredients)
 	if err != nil {
 		return -1, fmt.Errorf("Unable to insert ingredients: %v", err)
 	}
 
-	err = insertCustomIngredients(db, recipeId, recipe.CustomIngredients)
+	err = insertCustomIngredients(tx, recipeId, recipe.CustomIngredients)
 	if err != nil {
 		return -1, fmt.Errorf("Unable to insert custom ingredients: %v", err)
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return -1, fmt.Errorf("Unable to commit transaction: %v", err)
 	}
 
 	return recipeId, nil
 }
 
-func insertRecipe(db *sql.DB, recipe *models.Recipe) (int, error) {
+func insertRecipe(tx *sql.Tx, recipe *models.Recipe) (int, error) {
 	var recipeId int
 	query := "INSERT INTO recipes (name, unit, username) VALUES ($1, $2, $3) RETURNING id"
-	err := db.QueryRow(query, recipe.Name, recipe.Unit, recipe.Username).Scan(&recipeId)
+	err := tx.QueryRow(query, recipe.Name, recipe.Unit, recipe.Username).Scan(&recipeId)
 	if err != nil {
 		return -1, err
 	}
 	return recipeId, nil
 }
 
-func insertCustomIngredients(db *sql.DB, recipeId int, customIngredients []models.CustomIngredient) error {
+func insertCustomIngredients(tx *sql.Tx, recipeId int, customIngredients []models.CustomIngredient) error {
 	query := "INSERT INTO custom_ingredients (recipe_id, custom_ingredient_id, quantity) VALUES ($1, $2, $3)"
 	for _, ingredient := range customIngredients {
-		_, err := db.Exec(query, recipeId, ingredient.CustomIngredientId, ingredient.Quantity)
+		_, err := tx.Exec(query, recipeId, ingredient.CustomIngredientId, ingredient.Quantity)
 		if err != nil {
 			return err
 		}
@@ -47,10 +58,10 @@ func insertCustomIngredients(db *sql.DB, recipeId int, customIngredients []model
 	return nil
 }
 
-func insertIngredients(db *sql.DB, recipeId int, ingredients []models.Ingredient) error {
+func insertIngredients(tx *sql.Tx, recipeId int, ingredients []models.Ingredient) error {
 	query := "INSERT INTO ingredients (recipe_id, ingredient_id, quantity) VALUES ($1, $2, $3)"
 	for _, ingredient := range ingredients {
-		_, err := db.Exec(query, recipeId, ingredient.IngredientId, ingredient.Quantity)
+		_, err := tx.Exec(query, recipeId, ingredient.IngredientId, ingredient.Quantity)
 		if err != nil {
 			return err
 		}
