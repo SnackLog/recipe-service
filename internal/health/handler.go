7c092e7c72a9856e082f895f37b370ada8a4865b
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2026-01-22 14:22:21 +0100

    Add .dockerignore and remove docs from health handler

--- internal/health/handler.go
+++ internal/health/handler.go
@@ -3,13 +3,6 @@ package health
 import "github.com/gin-gonic/gin"
 
 // GetHealthStatus handles GET /health requests to check service health, pings the database
-// @Summary Health check
-// @Description Checks service health and database connection.
-// @Tags health
-// @Produce json
-// @Success 200 {object} map[string]string
-// @Failure 500 {object} map[string]string
-// @Router /recipe/health [get]
 func (hc *HealthController) GetHealthStatus(c *gin.Context) {
 	err := hc.DB.Ping()
 	if err != nil {
